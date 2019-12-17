49f90c83305b6fda2cb3564f50cd6707413f22ac
Me_Goes_RAWR <sammylover26@gmail.com>
2019-12-17 15:35:23 -0500

    Initial Upload
    Initial Upload

---
+++ Damage/DamageCMD.cs
@@ -0,0 +1,73 @@
+﻿using Rocket.API;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using System.Collections.Generic;
+
+namespace DamageShower
+{
+    public class DamageCMD : IRocketCommand
+    {
+        public List<string> Permissions
+        {
+            get
+            {
+                return new List<string>() { "damage.show" };
+            }
+        }
+
+        public AllowedCaller AllowedCaller
+        {
+            get
+            {
+                return AllowedCaller.Player;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "damage";
+            }
+        }
+
+        public string Syntax
+        {
+            get
+            {
+                return "";
+            }
+        }
+
+        public string Help
+        {
+            get
+            {
+                return "Damage measurement Command";
+            }
+        }
+
+        public List<string> Aliases
+        {
+            get
+            {
+                return new List<string>();
+            }
+        }
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            UnturnedPlayer p = (UnturnedPlayer)caller;
+            if (DamageShower.Instance.damage.Contains(p.Id))
+            {
+                DamageShower.Instance.damage.Remove(p.Id);
+                UnturnedChat.Say(p, "Damage measurement removed!");
+            } else
+            {
+                DamageShower.Instance.damage.Add(p.Id);
+                UnturnedChat.Say(p, "Damage measurement added!");
+            }
+            return;
+        }
+    }
+}
---
+++ Damage/DamageShower.cs
@@ -0,0 +1,74 @@
+﻿using Rocket.Core.Plugins;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using Logger = Rocket.Core.Logging.Logger;
+
+namespace DamageShower
+{
+    public class DamageShower : RocketPlugin
+    {
+        public static string version = "1.0.0";
+        public static string maker = "[RAR] Metshival";
+        public static DamageShower Instance;
+        public List<string> damage = new List<string>();
+
+        protected override void Load()
+        {
+            Instance = this;
+            Logger.Log("This Plugin was made by: " + maker);
+            Logger.Log("Version: " + version);
+            DamageTool.damagePlayerRequested += UnturnedEvents_OnPlayerDamaged;
+        }
+
+        private void UnturnedEvents_OnPlayerDamaged(ref DamagePlayerParameters pars, ref bool canDamage)
+        {
+            try
+            {
+                UnturnedPlayer player = UnturnedPlayer.FromPlayer(pars.player);
+                UnturnedPlayer killer = UnturnedPlayer.FromCSteamID(pars.killer);
+                if (damage.Contains(killer.Id))
+                {
+
+                    PlayerEquipment pe = killer.Player.equipment;
+                    Item item = killer.Inventory.getItem(pe.equippedPage, player.Inventory.getIndex(pe.equippedPage, pe.equipped_x, pe.equipped_y)).item;
+                    UnturnedChat.Say(killer, "Damage done: " + (pars.damage * pars.times) + ", Damaged Limb: " + LimbToName(pars.limb) + " and Weapon ID: " + item.id);
+                    canDamage = false;
+
+                }
+            }
+            catch (Exception) { }
+        }
+
+        public string LimbToName(ELimb limb)
+        {
+            string Part = "???";
+            if (limb == ELimb.SKULL)
+            {
+                Part = ("head");
+            }
+            else if (limb == ELimb.SPINE)
+            {
+                Part = ("body");
+            }
+            else if (limb == ELimb.LEFT_ARM || limb == ELimb.RIGHT_ARM || limb == ELimb.LEFT_HAND || limb == ELimb.RIGHT_HAND)
+            {
+                Part = ("arm");
+            }
+            else if (limb == ELimb.LEFT_LEG || limb == ELimb.RIGHT_LEG || limb == ELimb.LEFT_FOOT || limb == ELimb.RIGHT_FOOT)
+            {
+                Part = ("leg");
+            }
+            return Part;
+        }
+
+        protected override void Unload()
+        {
+            Logger.Log("This Plugin was made by: " + maker);
+            Logger.Log("Version: " + version);
+            DamageTool.damagePlayerRequested -= UnturnedEvents_OnPlayerDamaged;
+        }
+    }
+}
